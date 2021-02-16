c9b198512234a39d9af2b89be279911b710df0a8
hwangji-dev <melon2228@naver.com>
2021-02-16 23:25:43 +0900

    pushTest

---
+++ permissionCheck/permissionCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  permissionCheck
+//
+//  Created by 황지은 on 2021/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
