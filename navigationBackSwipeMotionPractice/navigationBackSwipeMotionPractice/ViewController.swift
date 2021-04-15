bc1d14b0e9d93077492dfdfa1671bae5a4f1d63e
hwangji-dev <melon2228@naver.com>
2021-04-15 23:56:25 +0900

    [Upload] navigationBar isHidden practice upload

--- navigationBackSwipeMotionPractice/navigationBackSwipeMotionPractice/ViewController.swift
+++ navigationBackSwipeMotionPractice/navigationBackSwipeMotionPractice/ViewController.swift
@@ -12,11 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationBackSwipeMotion()
+        navigationBarHidden()
         // Do any additional setup after loading the view.
     }
     
+    //MARK: - 네비게이션 백 스와이프 모션으로도 뒤로가기가 가능하도록 만들어주는 함수
     func navigationBackSwipeMotion() {
         self.navigationController?.interactivePopGestureRecognizer?.delegate = nil
+    }
+    
+    //MARK: - 네비게이션 바 숨기는 함수
+    func navigationBarHidden() {
+        self.navigationController?.navigationBar.isHidden = true
     }
 
 
