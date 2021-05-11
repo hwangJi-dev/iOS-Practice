e5669fd550803280d39546731b43de2762ebb3d0
hwangji-dev <melon2228@naver.com>
2021-05-12 04:05:13 +0900

    [upload]

---
+++ UIComponentPractice/UIComponentPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIComponentPractice
+//
+//  Created by 황지은 on 2021/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
