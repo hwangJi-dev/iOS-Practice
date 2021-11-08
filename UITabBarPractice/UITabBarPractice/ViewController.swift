69d5de98f44a96561311eb1de6aaeb09702025f5
hwangJi-dev <melon2228@swu.ac.kr>
2021-11-08 23:07:30 +0900

    tabBar init

---
+++ UITabBarPractice/UITabBarPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITabBarPractice
+//
+//  Created by 황지은 on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
