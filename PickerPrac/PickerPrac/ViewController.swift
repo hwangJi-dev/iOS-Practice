6e570890f401c9edd3bf5ead1d5f9de9428fa790
hwangJi-dev <melon2228@swu.ac.kr>
2021-12-12 23:44:18 +0900

    [chore] 프로젝트 수정

---
+++ PickerPrac/PickerPrac/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PickerPrac
+//
+//  Created by 황지은 on 2021/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var hiDatePicker: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        hiDatePicker.ho
+    }
+
+
+}
+
